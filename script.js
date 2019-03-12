13476c8f84ed20a7e547a86cff40a96b93952618
Tomer <tomerkobcherrypick@gmail.com>
2019-03-12 17:53:27 +0200

    added counter to work every time a new ToDo is Created

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+let itemCount = 0
+
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+	itemCount++
+	itemCountSpan.innerHTML = itemCount
+
+	//alert(itemCountSpan.innerHTML)
+  //alert('New TODO button clicked!')
 }
+
+function createToDo(){
+	const checkBox = document.createElement()
+}
+
+
